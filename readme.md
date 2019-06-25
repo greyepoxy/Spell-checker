7add3b0f16797850f36b60b192bbc75e12619fc1
Justin <greyepoxy2@gmail.com>
2019-06-25 00:18:37 -0700

    docs: add more examples

--- readme.md
+++ readme.md
@@ -8,7 +8,7 @@ Install a recent version of Node https://nodejs.org/en/ (developed against v11.1
 
 Import into an existing node application using a local file reference [npm local paths documents](https://docs.npmjs.com/files/package.json#local-paths)
 
-Then this package can be used
+Then this package can be used as follows
 
 ```ts
 import {readWordsFromFile, Spellchecker} from 'spellchecker';
@@ -26,6 +26,12 @@ Check out [the examples](./test/examples.ts) file for more usage examples
 1. Only works for English words
 1. Better performance could be achieved if needed
 1. This is needed on a server or in a desktop application (won't work in the browser as written)
+
+### Limitations
+
+#### Matching against multiple words with different casing
+
+When there are multiple words with just casing differences in the dictionary then the matching is a little arbitrary. Will match correctly if in the dictionary the word exists with the exact casing but if there is a slight casing mistake then will always prefer matching against the last version of the word loaded into the dictionary. Likely the desired behavior would be to match against the word with the fewest required casing transformations to be an exact match.
 
 ### How to build from source
 
--- test/examples.ts
+++ test/examples.ts
@@ -10,7 +10,32 @@ test.before(async () => {
   );
 });
 
-test('some lower case words that match', t => {
+test('exact matches return the original word', t => {
   t.deepEqual(spellchecker.checkWord('engender'), 'engender');
   t.deepEqual(spellchecker.checkWord('table'), 'table');
+  t.deepEqual(spellchecker.checkWord('Englander'), 'Englander');
+  t.deepEqual(spellchecker.checkWord('Parnassus'), 'Parnassus');
+});
+
+test('if no match for incoming word returns “No Correction Found”', t => {
+  t.deepEqual(spellchecker.checkWord('England'), 'No Correction Found');
+  t.deepEqual(spellchecker.checkWord('asdfasdfasdf'), 'No Correction Found');
+});
+
+test('fixes bad casing in original word', t => {
+  t.deepEqual(spellchecker.checkWord('paRNAssus'), 'Parnassus');
+  t.deepEqual(spellchecker.checkWord('vishnuvite'), 'Vishnuvite');
+  t.deepEqual(spellchecker.checkWord('MiSter'), 'mister');
+});
+
+test('removes repeating characters', t => {
+  t.deepEqual(spellchecker.checkWord('tabble'), 'table');
+  t.deepEqual(spellchecker.checkWord('rrreally'), 'really');
+  t.deepEqual(spellchecker.checkWord('miiiisttter'), 'mister');
+});
+
+test('removes repeating characters and fixes casing simultaneously', t => {
+  t.deepEqual(spellchecker.checkWord('paRNAsssus'), 'Parnassus');
+  t.deepEqual(spellchecker.checkWord('rrreAlly'), 'really');
+  t.deepEqual(spellchecker.checkWord('MiiiIstTter'), 'mister');
 });
